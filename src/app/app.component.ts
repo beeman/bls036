5607717ea1e324bc7317127579d3a8f446045f26
Bram Borggreve <borggreve@gmail.com>
2021-05-05 17:38:26 -0500

    feat: second episode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,103 @@
-import { Component } from '@angular/core'
-import { Mesh } from 'three'
-import { ThreeEvent } from '@angular-three/core'
+import { Component, Input } from '@angular/core'
+import * as THREE from 'three'
+import { LoaderService, ThreeVector3 } from '@angular-three/core'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 @Component({
   selector: 'app-root',
   template: `
-    <ngt-canvas>
-      <app-cube></app-cube>
+    <ngt-canvas
+      (created)="$event.gl.setClearColor('black')"
+      [camera]="{ position: [0, 0, 20] }"
+      [scene]="{ fog: fog }"
+      [shadows]="true"
+    >
+      <ngt-stats></ngt-stats>
+      <ngt-ambient-light></ngt-ambient-light>
+      <ngt-spot-light [castShadow]="true" [position]="[15, 20, 5]"></ngt-spot-light>
+      <app-control></app-control>
+
+      <app-spaceship></app-spaceship>
+      <!--      <app-cube *ngFor="let pos of positions" [position]="pos"></app-cube>-->
+
+      <!--      <app-plane></app-plane>-->
     </ngt-canvas>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  fog = new THREE.Fog('black', 15, 25)
+  positions = [
+    [1, 0, 0],
+    [-1, 0, 0],
+    [1, 2, 0],
+    [-1, 2, 0],
+  ]
+}
 
 @Component({
   selector: 'app-cube',
   template: `
     <ngt-mesh
+      [position]="position"
       [scale]="active ? [2, 2, 2] : [1, 1, 1]"
-      (click)="onClick($event)"
+      (click)="active = !active"
       (pointerover)="hover = true"
       (pointerout)="hover = false"
-      (animateReady)="animate($event.animateObject)"
+      [castShadow]="true"
     >
       <ngt-box-geometry> </ngt-box-geometry>
-      <ngt-mesh-basic-material [parameters]="{ color: hover ? '#00ff00' : '#ff0000' }"> </ngt-mesh-basic-material>
+      <ngt-mesh-physical-material [parameters]="{ color: hover ? '#00ff00' : '#ff0000' }"> </ngt-mesh-physical-material>
     </ngt-mesh>
   `,
 })
 export class CubeComponent {
+  @Input() position: ThreeVector3
   active = false
   hover = false
-  animate(cube: Mesh): void {
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+}
+
+@Component({
+  selector: 'app-control',
+  template: `
+    <ngt-orbit-controls (animateReady)="animate($event.animateObject)" (ready)="ready($event)"></ngt-orbit-controls>
+  `,
+})
+export class ControlComponent {
+  ready(controls: OrbitControls): void {
+    controls.autoRotate = true
+    controls.autoRotateSpeed = 5
+    // controls.maxPolarAngle = Math.PI / 3
+    // controls.minPolarAngle = Math.PI / 3
   }
 
-  onClick($event: ThreeEvent<MouseEvent>): void {
-    console.log($event)
-    this.active = !this.active
+  animate(animateObject: OrbitControls): void {
+    animateObject.update()
   }
+}
+
+@Component({
+  selector: 'app-plane',
+  template: `
+    <ngt-mesh [receiveShadow]="true" [rotation]="[-halfPI, 0, 0]" [position]="[0, -0.5, 0]">
+      <ngt-plane-geometry [args]="[100, 100]"></ngt-plane-geometry>
+      <ngt-mesh-physical-material [parameters]="{ color: 'white' }"> </ngt-mesh-physical-material>
+    </ngt-mesh>
+  `,
+})
+export class PlaneComponent {
+  halfPI = Math.PI / 2
+}
+
+@Component({
+  selector: 'app-spaceship',
+  template: `
+    <ng-container *ngIf="model$ | async as model">
+      <ngt-primitive [object]="model.scene"></ngt-primitive>
+    </ng-container>
+  `,
+})
+export class SpaceShipComponent {
+  model$ = this.service.use(GLTFLoader, 'assets/scene.gltf')
+  constructor(private readonly service: LoaderService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent, CubeComponent } from './app.component'
-import { ThreeCoreModule } from '@angular-three/core'
+import { AppComponent, ControlComponent, CubeComponent, PlaneComponent, SpaceShipComponent } from './app.component'
+import { ThreeCoreModule, ThreePrimitiveModule } from '@angular-three/core'
 import { ThreeMeshModule } from '@angular-three/core/meshes'
-import { ThreeBoxBufferGeometryModule } from '@angular-three/core/geometries'
-import { ThreeMeshBasicMaterialModule } from '@angular-three/core/materials'
+import { ThreeBoxBufferGeometryModule, ThreePlaneBufferGeometryModule } from '@angular-three/core/geometries'
+import { ThreeMeshBasicMaterialModule, ThreeMeshPhysicalMaterialModule } from '@angular-three/core/materials'
+import { ThreeOrbitControlsModule } from '@angular-three/controls/orbit-controls'
+import { ThreeAmbientLightModule, ThreeSpotLightModule } from '@angular-three/core/lights'
+import { ThreeStatsModule } from '@angular-three/core/stats'
 
 @NgModule({
-  declarations: [AppComponent, CubeComponent],
+  declarations: [AppComponent, CubeComponent, ControlComponent, PlaneComponent, SpaceShipComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -17,6 +20,13 @@ import { ThreeMeshBasicMaterialModule } from '@angular-three/core/materials'
     ThreeMeshModule,
     ThreeBoxBufferGeometryModule,
     ThreeMeshBasicMaterialModule,
+    ThreeOrbitControlsModule,
+    ThreeMeshPhysicalMaterialModule,
+    ThreeAmbientLightModule,
+    ThreeSpotLightModule,
+    ThreePlaneBufferGeometryModule,
+    ThreePrimitiveModule,
+    ThreeStatsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
